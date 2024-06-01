3f607b1e66fb13f9bae780840dcf55efd889acf6
ParthShah47210834 <100885264+parthshah2@users.noreply.github.com>
2024-06-01 23:24:15 +1000

    Refator: Implemented CSRF protection

--- project/__init__.py
+++ project/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 from pathlib import Path
+from flask_wtf.csrf import CSRFProtect 
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
+csrf = CSRFProtect() 
 
 def create_app():
     app = Flask(__name__)
@@ -15,6 +17,7 @@ def create_app():
     app.config['UPLOAD_DIR'] = CWD / "uploads"
 
     db.init_app(app)
+    csrf.init_app(app)
 
     # blueprint for non-auth parts of app
     from .main import main as main_blueprint
